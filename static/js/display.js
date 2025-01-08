7e68445f7af83360732375cd01c8c1d52ddca3b7
Tharusha <tharushadgunawardane@gmail.com>
2025-01-08 18:36:37 +0530

    Add drag and drop files feature

--- static/js/display.js
+++ static/js/display.js
@@ -73,3 +73,37 @@ function processImageOverlay(img) {
   // Update binary patterns after shifting
   updateBinaryPatterns();
 }
+
+function dropHandler(ev) {
+  // Prevent default behavior (Prevent file from being opened)
+  ev.preventDefault();
+
+  if (ev.dataTransfer.items) {
+    // Use DataTransferItemList interface to access the file(s)
+    [...ev.dataTransfer.items].forEach((item, i) => {
+      // If dropped items aren't files, reject them
+      if (item.kind === "file") {
+        const file = item.getAsFile();
+        const reader = new FileReader();
+        reader.onload = function (e) {
+          const img = new Image();
+          img.src = e.target.result;
+    
+          img.onload = () => {
+            currentImage = img; // Store the current image
+            processImageOverlay(img); // Process the uploaded image
+          };
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  } else {
+    // Use DataTransfer interface to access the file(s)
+    [...ev.dataTransfer.files].forEach((file, i) => {
+    });
+  }
+}
+
+function dragOverHandler(ev) {
+  ev.preventDefault();
+}
