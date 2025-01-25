9c26e33c1b4c4938bf89412072688fa63d3b6308
Tharusha <tharushadgunawardane@gmail.com>
2025-01-26 00:48:47 +0530

    resolve conflicting keydown listener in game

--- static/js/game.js
+++ static/js/game.js
@@ -1,18 +1,53 @@
-// General variables
+// Create grid
 
-const designs = [
-  [
-    ["0", "0", "0", "0", "0", "1", "0", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "1", "1", "1", "0", "0", "0", "0"],
-    ["0", "1", "0", "0", "1", "1", "1", "0", "0", "1", "0"], 
-    ["0", "1", "1", "1", "1", "1", "1", "1", "1", "1", "0"], 
-    ["1", "1", "1", "1", "1", "1", "1", "1", "1", "1", "1"],
-    ["0", "0", "0", "0", "1", "1", "1", "0", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "1", "0", "0", "0", "0", "0"], 
-    ["0", "0", "0", "1", "1", "1", "1", "1", "0", "0", "0"],
-    ["0", "0", "0", "0", "0", "1", "0", "0", "0", "0", "0"]
+const cells = []
+
+function createGrid() {
+  const totalRows = 64;
+  const totalCols = 32;
+  const batchSize = 8; // Number of rows to process per batch
+  let currentRow = 0;
+
+  // Function to process a batch of rows
+  function processBatch() {
+    for (let row = currentRow; row < currentRow + batchSize && row < totalRows; row++) {
+      cells[row] = [];
+      for (let col = 0; col < totalCols; col++) {
+        // Dynamically append buttons to the grid
+        $(".grid").append(`<button class="cell" data-row="${row}" data-col="${col}"></button>`);
+        cells[row][col] = $(`.cell[data-row=${row}][data-col="${col}"]`);
+      }
+    }
+    currentRow += batchSize;
+
+    if (currentRow < totalRows) {
+      // Schedule the next batch to run
+      setTimeout(processBatch, 0);
+    }
+  }
+
+  // Start the asynchronous grid creation
+  $(".grid").empty(); // Clear the grid before populating
+  processBatch();
+}
+
+createGrid();
+
+// Starting variables
+
+const spaceShipSkin = {
+  A: [
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0], 
+    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0], 
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], 
+    [0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
   ],
-]
+}
 
 let gameIsOn = true;
 
@@ -80,18 +115,67 @@ class Asteroid {
 }
 
 class SpaceShip {
-  constructor(design) {
-    this.design = design;
-    this.coords = design;
-    this.height = design.length;
-    this.width = design[0].length;
+  constructor(skin) {
+    this.skin = skin;
+    this.height = skin.length;
+    this.width = skin[0].length;
+
     this.xStart = Math.floor((32 - this.width) / 2); // The center of the grid
     this.yStart = 60;
+    this.coords = this.setSpawn(skin);
   }
 
-  draw() {
-    //fix this
-    console.log("");
+  setSpawn() {
+    let coords = structuredClone(this.skin);
+
+    for (let h=0; h<this.height; h++) {
+      for (let w=0; w<this.width; w++) {
+        const x = this.xStart + w;
+        const y = this.yStart - h;
+
+        coords[h][w] = [x, y];
+        
+        if (this.skin[this.height - 1 - h][this.width - 1 - w]) {
+          cells[y][x].addClass("cell--active");
+        }
+      }
+    }
+    
+    return coords;
+  }
+
+  move(key) {
+
+    console.log(key);
+
+    for (let h=0; h<this.height; h++) {
+      for (let w=0; w<this.width; w++) {
+        const x = this.coords[h][w][0];
+        const y = this.coords[h][w][1];
+        
+        if (this.skin[this.height - 1 - h][this.width - 1 - w]) {
+          cells[y][x].removeClass("cell--active");
+        }
+
+        let newX = x + 1;
+        if (newX > 31) {
+          newX = 0;
+        }
+
+        this.coords[h][w] = [newX, y];
+      }
+    }
+
+    for (let h=0; h<this.height; h++) {
+      for (let w=0; w<this.width; w++) {
+        const x = this.coords[h][w][0];
+        const y = this.coords[h][w][1];
+
+        if (this.skin[this.height - 1 - h][this.width - 1 - w]) {
+          cells[y][x].addClass("cell--active");
+        }
+      }
+    }
   }
 }
 
@@ -144,12 +228,14 @@ async function startAsteroidShower() {
 
 async function initializeSpaceShip() {
   await delay(2000) // Allows the grid to load;
+  
+  const spaceShip = new SpaceShip(spaceShipSkin['A']);
 
-  const spaceShip = new SpaceShip(designs[0]);
-  spaceShip.draw();
+  $(document).on("keydown", ({which}) => {
+    spaceShip.move(which);
+  });
 
   while (gameIsOn) {
     await delay(100);
-    console.log(123);
   }
 }
