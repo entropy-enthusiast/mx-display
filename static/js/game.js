bc9429205ede6d70fe4c32e0f1eb1a6a1fd8926e
Tharusha <tharushadgunawardane@gmail.com>
2025-02-19 05:28:51 +0530

    add collision functions

--- static/js/game.js
+++ static/js/game.js
@@ -128,9 +128,9 @@ let spaceShipSkin = {
       [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
     ],
     weapons: [
-      [34, 11],
-      [34, 19],
-      [31, 15]
+      [2, 11],
+      [2, 19],
+      [0, 15]
     ],
     front: {
       y: [4, 2, 3, 3, 1, 0, 1, 3, 3, 2, 4],
@@ -140,35 +140,38 @@ let spaceShipSkin = {
 
   B: {
     body: [
-      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
-      [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1],
-      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-      [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
       [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      [1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+      [0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+      [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
     ],
     weapons: [
-      [34, 22],
-      [34, 9],
+      [7, 22],
+      [7, 9],
     ],
     front: {
-      y: [4, 5, 5, 5, 5, 3, 0, 3, 5, 5, 5, 5, 5, 4],
+      y: [5, 4, 4, 4, 4, 6, 9, 9, 6, 4, 4, 4, 4, 5],
       x: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     }
   }
 }
 
-let player = spaceShipSkin['B'];
+let player = spaceShipSkin['A'];
+
+let fight = false;
+let boundary = 0;
 
 let gameIsOn = true;
-let fight = false;
 let asteroids = [];
 let asteroidSpeed = 500;
 let bullets = [];
+let enemyBullets = [];
 let firstLoad = true;
 let score = 0;
 
@@ -238,16 +241,16 @@ class Asteroid {
 }
 
 class SpaceShip {
-  constructor(skin) {
+  constructor(skin, yStart) {
     this.skin = skin["body"];
     this.height = this.skin.length;
     this.width = this.skin[0].length;
 
     this.xStart = Math.floor((32 - this.width) / 2); // The center of the grid
-    this.yStart = 40;
+    this.yStart = yStart;
     this.coords = this.setSpawn();
 
-    this.weapons = skin["weapon"];
+    this.weapons = skin["weapons"];
   }
 
   setSpawn() {
@@ -270,19 +273,18 @@ class SpaceShip {
   }
 
   move(key) {
-
     if (key === 68) {
-      player["weapons"].forEach((weapon, i) => {
-        player["weapons"][i][1] = weapon[1] + 1;
+      this.weapons.forEach((weapon, i) => {
+        this.weapons[i][1] = weapon[1] + 1;
         if (weapon[1] > 31) {
-          player["weapons"][i][1] = 0;
+          this.weapons[i][1] = 0;
         }
       });
     } else if (key === 65) {
-      player["weapons"].forEach((weapon, i) => {
-        player["weapons"][i][1] = weapon[1] - 1;
+      this.weapons.forEach((weapon, i) => {
+        this.weapons[i][1] = weapon[1] - 1;
         if (weapon[1] < 0) {
-          player["weapons"][i][1] = 31;
+          this.weapons[i][1] = 31;
         }
       });
     }
@@ -331,9 +333,9 @@ class SpaceShip {
 }
 
 class Bullet {
-  constructor(x, y) {
+  constructor(x, y, start) {
     this.xPos = x;
-    this.yPos = y;
+    this.yPos = start + y;
     this.outOfBounds = false;
   }
 
@@ -345,13 +347,20 @@ class Bullet {
     cells[y][x].removeClass("cell--active");
   }
 
-  move() {
+  move(boundary, dir=1) {
     if (this.yPos != -1) {
       this.erase();
     }
 
-    if (this.yPos > 0) {
-      this.yPos -= 1;
+    let condition;
+    if (dir === -1) {
+      condition = this.yPos < boundary;
+    } else {
+      condition = this.yPos > boundary;
+    }
+
+    if (condition) {
+      this.yPos -= dir;
       this.draw();
     } else {
       this.outOfBounds = true;
@@ -388,6 +397,7 @@ $(document).ready(() => {
   $("#fightBtn").on("click", () => {
     if (firstLoad) {
       fight = true;
+      boundary = 9;
       handleSpaceShip();
     }
 
@@ -508,12 +518,18 @@ async function handleSpaceShip() {
     });
   }
 
-  const spaceShip = new SpaceShip(player);
+  let spaceShip;
+  if (fight) {
+    spaceShip = new SpaceShip(player, 52);
+    enemySpaceShip = new SpaceShip(spaceShipSkin["B"], 20);
+  } else {
+    spaceShip = new SpaceShip(player, 40);
+  }
 
   $(document).on("keydown", ({which}) => {
     if (which === 32) {
       player["weapons"].forEach(weapon => {
-        const bullet = new Bullet(weapon[1], weapon[0]);
+        const bullet = new Bullet(weapon[1], weapon[0], spaceShip.yStart - spaceShip.skin.length);
         bullets.push(bullet);
       });
     } else {
@@ -522,47 +538,133 @@ async function handleSpaceShip() {
   });
 
   while (gameIsOn) {
-    await delay(50);
+    if (fight) {
+      await delay(300);
+    } else {
+      await delay(50);
+    }
 
     handleBinaryPattern();
 
     // Move bullets
     bullets.forEach(bullet => {
-      bullet.move();
+      bullet.move(boundary);
     });
 
-    // Delete asteroids that go out of the grid
-    bullets = Bullet.cleanSpace(bullets);    
+    // Delete bullets that go out of the grid
+    bullets = Bullet.cleanSpace(bullets);
 
+    if (fight) {
+      // Move enemy spaceship
+      const dirChange = getRandomInt(0, 100);
+      if (dirChange < 75) { // 75% chance of being still
+      } else if (dirChange < 87.5) { // 12.5% chance of moving left
+        enemySpaceShip.move(65);
+      } else { // 12.5% chance of moving right
+        enemySpaceShip.move(68);
+      }
+
+      // Shoot enemy bullets
+      // if (getRandomInt(0, 100) > 75) {
+      //   spaceShipSkin["B"]["weapons"].forEach(weapon => {
+      //     const bullet = new Bullet(weapon[1], weapon[0], enemySpaceShip.yStart - enemySpaceShip.skin.length);
+      //     enemyBullets.push(bullet);
+      //   });
+      // }
+
+      enemyBullets.forEach(bullet => {
+        bullet.move(54, -1);
+      });
+
+      enemyBullets = Bullet.cleanSpace(enemyBullets);  
+
+      const enemyBulletsCopy = [1, ...enemyBullets];
+      const playerBulletsCopy = [...bullets];
+      
+      const enemyIndexesToRemove = new Set();
+      const playerIndexesToRemove = new Set();
+      
+      for (let i = enemyBulletsCopy.length - 1; i >= 0; i--) {
+        const eX = enemyBulletsCopy[i].xPos;
+        const eY = enemyBulletsCopy[i].yPos;
+        let pX;
+        let pY;
+
+        for (let j = playerBulletsCopy.length - 1; j >= 0; j--) {
+          const pX = playerBulletsCopy[j].xPos;
+          const pY = playerBulletsCopy[j].yPos;
+      
+          if (eX === pX && eY === pY) {
+            enemyIndexesToRemove.add(i);
+            playerIndexesToRemove.add(j);
+            cells[pY][pX].removeClass("cell--active");
+          }
+        }
+
+        for (let k=0; k < spaceShipSkin["B"]["front"]["x"].length; k++) {
+          const sX = spaceShipSkin["B"]["front"]["x"][k];
+          const sY = spaceShipSkin["B"]["front"]["y"][k];
+    
+          const cX = enemySpaceShip.coords[enemySpaceShip.coords.length - 1 - sY][sX][0];
+          const cY = enemySpaceShip.coords[enemySpaceShip.coords.length - 1 - sY][sX][1];
+
+          if ((cX === pX) && (cY === pY)) {
+            gameIsOn = false;
+            console.log("Game Over!");
+            return;
+          }
+        }
+
+        // SpaceShip collisions
+        // for (let k=0; k < player["front"]["x"].length; k++) {
+        //   const sX = player["front"]["x"][k];
+        //   const sY = player["front"]["y"][k];
+    
+        //   const cX = spaceShip.coords[spaceShip.coords.length - 1 - sY][sX][0];
+        //   const cY = spaceShip.coords[spaceShip.coords.length - 1 - sY][sX][1];
+      
+        //   if ((cX === eX) && (cY === eY)) {
+        //     gameIsOn = false;
+        //     console.log("Game Over!");
+        //     return;
+        //   }
+        // }
+      }
+      
+      // Remove marked bullets safely after the loop
+      enemyBullets = enemyBullets.filter((_, index) => !enemyIndexesToRemove.has(index));
+      bullets = bullets.filter((_, index) => !playerIndexesToRemove.has(index));
+
+  } else {
     // Handle collisions
     const asteroidsCopy = [...asteroids];
     const bulletsCopy = [...bullets];
-
+  
     for (let i = 0; i < asteroidsCopy.length; i++) {
       const aX = asteroidsCopy[i].xPos;
       const aY = asteroidsCopy[i].yPos;
-
+  
       for (let j = 0; j < bulletsCopy.length; j++) {
         bX = bulletsCopy[j].xPos
         bY = bulletsCopy[j].yPos;
         if ((aX === bX) && (aY === bY)) {
           asteroids = asteroids.filter((_, index) => index != i);
           bullets = bullets.filter((_, index) => index != j);
-
+  
           cells[bY][bX].removeClass("cell--active");
           score += 1;
           updateScore(score);
-
+  
           if (asteroidSpeed > 0) {
             asteroidSpeed -= 1;
           }
         }
       }
-
+  
       for (let k=0; k < player["front"]["x"].length; k++) {
         const sX = player["front"]["x"][k];
         const sY = player["front"]["y"][k];
-
+  
         const cX = spaceShip.coords[spaceShip.coords.length - 1 - sY][sX][0];
         const cY = spaceShip.coords[spaceShip.coords.length - 1 - sY][sX][1];
     
@@ -573,6 +675,8 @@ async function handleSpaceShip() {
         }
       }
     }
+  }
+
   }
 }
 
